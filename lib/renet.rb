2cad4d452e821cdb428634baa6828b00425d2ae3
Job Vranish <job.vranish@gmail.com>
2015-01-03 18:45:14 -0500

    fixed a silly issue when using the git repo directly from bundler

--- lib/renet.rb
+++ lib/renet.rb
@@ -1,16 +1,12 @@
 if File.exists?("#{File.dirname(__FILE__)}/renet.bundle")
-	require "#{File.dirname(__FILE__)}/renet.bundle"
-end
- 
-if File.exists?("#{File.dirname(__FILE__)}/renet.so")
-	require "#{File.dirname(__FILE__)}/renet.so"
-end
-
-if File.exists?("#{File.dirname(__FILE__)}/renet.1.8.so")
-	if defined? RUBY_VERSION and RUBY_VERSION[0..2] == '1.9' then
-		version = '1.9'
-	else
-		version = '1.8'
-	end
-	require "#{File.dirname(__FILE__)}/renet.#{version}.so"
+  require "#{File.dirname(__FILE__)}/renet.bundle"
+elsif File.exists?("#{File.dirname(__FILE__)}/renet.so")
+  require "#{File.dirname(__FILE__)}/renet.so"
+elsif File.exists?("#{File.dirname(__FILE__)}/renet.1.8.so")
+  if defined? RUBY_VERSION and RUBY_VERSION[0..2] == '1.9' then
+    version = '1.9'
+  else
+    version = '1.8'
+  end
+  require "#{File.dirname(__FILE__)}/renet.#{version}.so"
 end
